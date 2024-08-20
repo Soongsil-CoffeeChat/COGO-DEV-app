4ee1eedfe4dba3a0fdd1ba7ad5331c194d45ddd1
DevDAN09 <devyun09@gmail.com>
2024-08-20 14:23:09 +0900

    feat: colors.dart

---
+++ lib/constants/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+final class CogoColor {
+  // Brand Color
+  static const main = Color(0xFF000000);
+  static const sub1 = Color(0xFFF4F4F4);
+  static const sub2 = Color(0xFF9C9C9C);
+
+  /// Semantic Color
+  static const text = Color(0xFF000000);
+  static const secondaryText = Color(0xFFAEAEB2);
+
+  /// Button Colors : Button Text
+  static const buttonText = Color(0xFFFFFFFF);
+  static const invertedButtonText = Color(0xFF000000);
+
+  /// Button Colors : Button Background
+  static const buttonBackground = Color(0xFFFFFFFF);
+  static const invertedButtonBackground = Color(0xFF000000);
+
+  /// Backgrounds
+  static const background = Color(0xFFFFFFFF);
+  static const secondaryBackground = Color(0xFFF6F6F6);
+  static const inputBackground = Color(0xFFF4F4F4);
+
+  static const shadow = Colors.black12;
+  static const imagePlaceholder = Color(0xFFD9D9D9);
+}
