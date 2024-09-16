2fd88a3c52d45f6c7d5164d937788cf152255d76
Yu Jin <qldls0307@naver.com>
2024-09-16 20:34:11 +0900

    modify: 코드 리뷰 반영

--- lib/data/api_client.dart
+++ lib/data/api_client.dart
@@ -1,13 +1,14 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_config/flutter_config.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ApiClient {
   //싱글톤으로 인스턴스 생성
   static final ApiClient _instance = ApiClient._internal();
   late Dio _dio;
 
-  final FlutterSecureStorage _storage = FlutterSecureStorage();
+  // final FlutterSecureStorage _storage = FlutterSecureStorage();
 
   factory ApiClient() {
     return _instance;
@@ -29,8 +30,9 @@ class ApiClient {
       onRequest: (options, handler) async {
         // final token = await _storage.read(key: 'auth_token');
 
-        const token =
-            "eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsInVzZXJuYW1lIjoiZ29vZ2xlIDExNDk2MjE2MTU2OTAwNTk0NzY2OCIsInJvbGUiOiJST0xFX01FTlRPUiIsImlhdCI6MTcyNTg3MjY1OCwiZXhwIjoxNzI3NjcyNjU4fQ.2IO784c9CD3lpxpii9U2iFpbD-zaum-dupFeeEU2RAg";
+        // 현재 token 발급 api가 없으므로 FlutterConfig에 저장한 임시 토큰을 끼웁니다.
+        // 추후 FlutterSecureStorage로 이관합니다.
+        var token = FlutterConfig.get("mento_token");
         if (token != null) {
           options.headers['Authorization'] = 'Bearer $token';
         }
@@ -39,12 +41,12 @@ class ApiClient {
       },
       onResponse: (response, handler) {
         // 응답 처리
-        print('응답은요 : ${response.statusCode}');
+        log('응답은요 : ${response.statusCode}');
         return handler.next(response);
       },
       onError: (DioException e, handler) {
         // 에러 처리
-        print('Error: ${e.message}');
+        log('Error: ${e.message}');
         return handler.next(e);
       },
     ));
@@ -52,69 +54,16 @@ class ApiClient {
     // LogInterceptor 추가
     _dio.interceptors.add(LogInterceptor(
       request: true,
-      // 요청 정보를 로그로 출력
       requestHeader: true,
-      // 요청 헤더 정보를 로그로 출력
       requestBody: true,
-      // 요청 본문 정보를 로그로 출력
       responseHeader: true,
-      // 응답 헤더 정보를 로그로 출력
       responseBody: true,
-      // 응답 본문 정보를 로그로 출력
       error: true,
-      // 에러 정보를 로그로 출력
       logPrint: (obj) {
-        // 원하는 로그 출력 방식 정의 (기본은 print)
-        print("서버통신 $obj");
+        log("서버통신 $obj");
       },
     ));
   }
 
   Dio get dio => _dio;
-
-
-//   // GET 요청 메서드
-//   Future<Response> get(String path,
-//       {Map<String, dynamic>? queryParameters}) async {
-//     return await _dio.get(path, queryParameters: queryParameters);
-//   }
-//
-//   // POST 요청 메서드
-//   Future<Response> post(String path, {dynamic data}) async {
-//     return await _dio.post(path, data: data);
-//   }
-//
-// // 다른 HTTP 메서드들도 필요에 따라 추가할 수 있습니다.
-//
-// // 회원가입 메서드
-//   Future<bool> signUp(String email, String password) async {
-//     try {
-//       final response = await _dio.post('/auth/signup', data: {
-//         'email': email,
-//         'password': password,
-//       });
-//
-//       if (response.statusCode == 200 || response.statusCode == 201) {
-//         // 토큰 저장
-//         final token = response.data['token'];
-//         await _storage.write(key: 'auth_token', value: token);
-//         return true;
-//       } else {
-//         return false;
-//       }
-//     } catch (e) {
-//       print('Sign up error: $e');
-//       return false;
-//     }
-//   }
-//
-//   // 토큰 가져오기
-//   Future<String?> getToken() async {
-//     return await _storage.read(key: 'auth_token');
-//   }
-//
-//   // 토큰 삭제 (로그아웃 시 사용)
-//   Future<void> deleteToken() async {
-//     await _storage.delete(key: 'auth_token');
-//   }
 }
