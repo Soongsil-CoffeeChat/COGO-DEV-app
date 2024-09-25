31ca2b1e9d6b50d11dac76e02e290edb1e752e67
Seoneui <ellieella2031@gmail.com>
2024-09-25 16:18:06 +0900

    fix : 키보드 오버플로우 해결
    resizeToAvoidBottomInset 추가

--- lib/features/auth/signup/views/shared_views/phone_number_screen.dart
+++ lib/features/auth/signup/views/shared_views/phone_number_screen.dart
@@ -14,6 +14,7 @@ class PhoneNumberVerificationScreen extends StatelessWidget {
       create: (_) => PhoneNumberVerificationViewModel(),
       child: Scaffold(
         backgroundColor: Colors.white,
+        resizeToAvoidBottomInset: true,  // 키도브 오버 플로우 해결
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
