9bf94e07a213fbef2f7372c208a75049c6a4f408
Yu Jin <qldls0307@naver.com>
2024-10-08 01:02:38 +0900

    chore: changed new logo

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -40,7 +40,7 @@ final AppRouter = GoRouter(
         path: Paths.splash,
         pageBuilder: (context, state) => MaterialPage(
               key: state.pageKey,
-              child: const SplashScreen(),
+              child: SplashScreen(),
             )),
     GoRoute(
       path: Paths.login,
