edd0e8d426b2f8517bb63d01369773ed8ed1f707
Yu Jin <qldls0307@naver.com>
2024-10-08 21:40:50 +0900

    chore: changing package

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -1,14 +1,14 @@
 import 'package:cogo/common/navigator/view/bottom_navigation_bar.dart';
 import 'package:cogo/constants/paths.dart';
 import 'package:cogo/features/auth/login/login_screen.dart';
-import 'package:cogo/features/auth/signup/views/mentee/interest_selection_screen.dart';
-import 'package:cogo/features/auth/signup/views/mentor/club_selection_screen.dart';
-import 'package:cogo/features/auth/signup/views/mentor/interest_selection_screen.dart';
-import 'package:cogo/features/auth/signup/views/mentor/mentor_info_screen.dart';
-import 'package:cogo/features/auth/signup/views/shared_views/agreement_screen.dart';
-import 'package:cogo/features/auth/signup/views/shared_views/choose_role_screen.dart';
-import 'package:cogo/features/auth/signup/views/shared_views/name_input_screen.dart';
-import 'package:cogo/features/auth/signup/views/shared_views/phone_number_screen.dart';
+import 'package:cogo/features/auth/signup/agreement/agreement_screen.dart';
+import 'package:cogo/features/auth/signup/choose_role/choose_role_screen.dart';
+import 'package:cogo/features/auth/signup/club/club_selection_screen.dart';
+import 'package:cogo/features/auth/signup/completion/completion_screen.dart';
+import 'package:cogo/features/auth/signup/interest/interest_selection_screen.dart';
+import 'package:cogo/features/auth/signup/mento_info/mentor_info_screen.dart';
+import 'package:cogo/features/auth/signup/name_input/name_input_screen.dart';
+import 'package:cogo/features/auth/signup/phone_number/phone_number_screen.dart';
 import 'package:cogo/features/cogo/views/mentor/cogo_screen.dart';
 import 'package:cogo/features/cogo/views/mentor/received_cogo_detail_screen.dart';
 import 'package:cogo/features/cogo/views/mentor/received_cogo_screen.dart';
@@ -20,12 +20,10 @@ import 'package:cogo/features/home/apply/views/schedule_screen.dart';
 import 'package:cogo/features/home/home/view/home_screen.dart';
 import 'package:cogo/features/home/profile/view/profile_detail_screen.dart';
 import 'package:cogo/features/home/search/view/search_screen.dart';
+import 'package:cogo/features/mypage/profile_management/views/mentor_introduction_screen.dart';
 import 'package:cogo/features/mypage/views/mypage_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:cogo/features/mypage/profile_management/views/mentor_introduction_screen.dart';
-
-import '../features/auth/signup/views/shared_views/completion_screen.dart';
 
 final AppRouter = GoRouter(
   // initialLocation: '/',
@@ -66,10 +64,10 @@ final AppRouter = GoRouter(
           ),
         ),
         GoRoute(
-          path: Paths.mentorInterest,
+          path: Paths.interest,
           pageBuilder: (context, state) => MaterialPage(
             key: state.pageKey,
-            child: const MentorInterestSelectionScreen(),
+            child: const InterestSelectionScreen(),
           ),
         ),
         GoRoute(
@@ -84,13 +82,6 @@ final AppRouter = GoRouter(
           pageBuilder: (context, state) => MaterialPage(
             key: state.pageKey,
             child: const MentorInfoScreen(),
-          ),
-        ),
-        GoRoute(
-          path: Paths.menteeInterest,
-          pageBuilder: (context, state) => MaterialPage(
-            key: state.pageKey,
-            child: const MenteeInterestSelectionScreen(),
           ),
         ),
         GoRoute(
