faca203bc74816553216d3459417bc3d9719b6ca
Yu Jin <qldls0307@naver.com>
2024-10-08 21:40:24 +0900

    feat: write userservice method

---
+++ lib/common/enums/role.dart
@@ -0,0 +1,5 @@
+enum Role {
+  mentor,
+  mentee,
+  user,
+}
--- lib/data/service/user_service.dart
+++ lib/data/service/user_service.dart
@@ -1,7 +1,10 @@
 import 'package:cogo/constants/apis.dart';
 import 'package:cogo/data/di/api_client.dart';
 import 'package:cogo/data/dto/response/base_response.dart';
+import 'package:cogo/data/dto/response/mentee_signup_response.dart';
+import 'package:cogo/data/dto/response/mentor_signup_response.dart';
 import 'package:cogo/data/dto/response/sms_verification_response.dart';
+import 'package:cogo/data/dto/response/user_info_response.dart';
 import 'package:dio/dio.dart';
 
 class UserService {
@@ -35,6 +38,95 @@ class UserService {
       throw Exception('Error: ${e.response?.data ?? e.message}');
     } catch (e) {
       // 기타 모든 예외 처리
+      throw Exception('An unexpected error occurred: $e');
+    }
+  }
+
+  //POST /api/v2/users 기본정보 기입
+  Future<UserInfoResponse> setUserInfo(String phoneNumber, String name) async {
+    try {
+      final response = await _apiClient.dio.get(
+        options: Options(
+          extra: {'skipAuthToken': false}, //토큰 해제
+        ),
+        apiVersion + Apis.userInfo,
+        data: {
+          'phoneNum': phoneNumber,
+          'name': name,
+        },
+      );
+      if (response.statusCode == 200) {
+        //base response로 받는건 여기서 뿐임.
+        final baseResponse = BaseResponse<UserInfoResponse>.fromJson(
+          response.data,
+          (contentJson) => UserInfoResponse.fromJson(contentJson),
+        );
+        return baseResponse.content;
+      } else {
+        throw Exception('Failed to send verification code ${response.data}');
+      }
+    } on DioException catch (e) {
+      throw Exception('Error: ${e.response?.data ?? e.message}');
+    } catch (e) {
+      throw Exception('An unexpected error occurred: $e');
+    }
+  }
+
+  //POST /api/v2/users/mentor  멘토로 가입하기!
+  Future<MentorSignupResponse> signUpMentor(String part, String club) async {
+    try {
+      final response = await _apiClient.dio.get(
+        options: Options(
+          extra: {'skipAuthToken': false}, //토큰 해제
+        ),
+        apiVersion + Apis.signUpMentor,
+        data: {
+          'part': part,
+          'club': club,
+        },
+      );
+      if (response.statusCode == 200) {
+        //base response로 받는건 여기서 뿐임.
+        final baseResponse = BaseResponse<MentorSignupResponse>.fromJson(
+          response.data,
+          (contentJson) => MentorSignupResponse.fromJson(contentJson),
+        );
+        return baseResponse.content;
+      } else {
+        throw Exception('Failed to send verification code ${response.data}');
+      }
+    } on DioException catch (e) {
+      throw Exception('Error: ${e.response?.data ?? e.message}');
+    } catch (e) {
+      throw Exception('An unexpected error occurred: $e');
+    }
+  }
+
+  //POST /api/v2/users/mentee  멘티로 가입하기!
+  Future<MenteeSignupResponse> signUpMentee(String part) async {
+    try {
+      final response = await _apiClient.dio.get(
+        options: Options(
+          extra: {'skipAuthToken': false}, //토큰 해제
+        ),
+        apiVersion + Apis.signUpMentee,
+        data: {
+          'part': part,
+        },
+      );
+      if (response.statusCode == 200) {
+        //base response로 받는건 여기서 뿐임.
+        final baseResponse = BaseResponse<MenteeSignupResponse>.fromJson(
+          response.data,
+          (contentJson) => MenteeSignupResponse.fromJson(contentJson),
+        );
+        return baseResponse.content;
+      } else {
+        throw Exception('Failed to send verification code ${response.data}');
+      }
+    } on DioException catch (e) {
+      throw Exception('Error: ${e.response?.data ?? e.message}');
+    } catch (e) {
       throw Exception('An unexpected error occurred: $e');
     }
   }
