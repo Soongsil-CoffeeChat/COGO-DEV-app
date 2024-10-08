347f42a7abb5434bf271da204a25474f551cf64d
Yu Jin <qldls0307@naver.com>
2024-10-08 10:43:51 +0900

    chore: add remove

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cogo/route/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'common/db/locale_manager.dart';
@@ -8,13 +9,14 @@ import 'data/di/get_it_locator.dart';
 
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
   await FlutterConfig.loadEnvVariables(); // env
   await LocaleManager.preferencesInit();  // SharedPreferences 초기화
   await initializeDateFormatting('ko_KR', null);
 
   setupServiceLocator(); //get it
-
   runApp(const MyApp());
 }
 
