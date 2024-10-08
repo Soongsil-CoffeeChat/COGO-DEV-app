7394370eede0ac06e88c8586078c26246cc9b677
Yu Jin <qldls0307@naver.com>
2024-10-08 21:40:50 +0900

    chore: changing package

--- lib/data/di/get_it_locator.dart
+++ lib/data/di/get_it_locator.dart
@@ -1,7 +1,7 @@
 import 'package:cogo/data/service/refresh_service.dart';
 import 'package:cogo/data/service/user_service.dart';
 import 'package:cogo/features/auth/login/login_view_model.dart';
-import 'package:cogo/features/auth/signup/view_models/shared_views/phone_number_view_model.dart';
+import 'package:cogo/features/auth/signup/phone_number/phone_number_view_model.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
--- lib/features/auth/signup/name_input/name_input_view_model.dart
+++ lib/features/auth/signup/name_input/name_input_view_model.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:cogo/common/db/locale_manager.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class SignupNameViewModel extends ChangeNotifier {
