ae6124289298cef249c4cfec991fd95520056314
sunnny619 <ellieella2031@gmail.com>
2024-10-16 16:33:35 +0900

    fix: initialLocation 경로 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,21 @@
-import 'package:cogo/common/navigator/view_model/bottom_navigation_bar_view_model.dart';
-import 'package:cogo/features/home/home/view_model/home_view_model.dart';
 import 'package:cogo/route/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:provider/provider.dart';
 
 import 'common/db/locale_manager.dart';
 import 'data/di/get_it_locator.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterConfig.loadEnvVariables(); // env
-  await LocaleManager.preferencesInit(); // SharedPreferences 초기화
+  await LocaleManager.preferencesInit();  // SharedPreferences 초기화
   await initializeDateFormatting('ko_KR', null);
 
   setupServiceLocator(); //get it
 
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(
-        create: (_) => BottomNavigationViewModel(AppRouter),
-      ),
-      ChangeNotifierProvider(
-        create: (_) => HomeViewModel(),
-      ),
-    ],
-    child: const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
