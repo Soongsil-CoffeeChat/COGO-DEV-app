c2685c89475c16e8bcc9b52ea0bf22da16bcb74b
Seoneui <ellieella2031@gmail.com>
2024-11-03 12:39:28 +0900

    chore: remove print

--- lib/features/home/home/view_model/home_view_model.dart
+++ lib/features/home/home/view_model/home_view_model.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cogo/data/dto/response/mentor_part_response.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -5,7 +7,6 @@ import 'package:cogo/constants/paths.dart';
 import 'package:cogo/data/service/mentor_service.dart';
 
 class HomeViewModel extends ChangeNotifier {
-  //String? selectedRole = LocaleManager.instance.getStringValue('selectedRole');
   String? selectedRole = "mentor";
   bool isIntroductionComplete = true; // 자기소개 완료 여부를 저장
 
@@ -13,20 +14,16 @@ class HomeViewModel extends ChangeNotifier {
 
   final MentorService _mentorService = MentorService(); // MentorService 객체 생성
 
-
   Future<void> onButtonPressed(BuildContext context, String title) async {
     final part = title; // API 호출에 사용할 mentorId
     try {
-      print('API 호출 시작');
       profiles = await _mentorService.getMentorPart(part);
-      print("API 호출$profiles");
       notifyListeners();
     } catch (error) {
       // 에러 처리 로직
-      print('Error fetching mentor details: $error');
+      log('Error fetching mentor details: $error');
     }
   }
-
 
   void onProfileCardTapped(BuildContext context, String mentorId) {
     context.push('${Paths.profileDetail}?mentorId=$mentorId');
