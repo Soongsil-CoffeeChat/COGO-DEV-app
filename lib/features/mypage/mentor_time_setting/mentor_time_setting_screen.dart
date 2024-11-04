f277d129a16a03f2cc6e990f6deeebc549ff2844
유진 <qldls0307@naver.com>
2024-11-04 21:10:33 +0900

    [Feat] 마이페이지 뷰 (#46)
    * feat: 마이페이지 뷰
    
    * add: mypage files
    
    * feat: routing
    
    * chore: change role

---
+++ lib/features/mypage/mentor_time_setting/mentor_time_setting_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MentorTimeSettingScreen extends StatelessWidget {
+  const MentorTimeSettingScreen({super.key});
+
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('MentorTimeSetting Screen')),
+    );
+  }
+}
