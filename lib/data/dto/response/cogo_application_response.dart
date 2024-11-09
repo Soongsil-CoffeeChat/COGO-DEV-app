5d5b72bda2610b26058c8b61ab1ba6a5f206fbfb
Seoneui Jee <146940671+sunnny619@users.noreply.github.com>
2024-11-09 22:28:49 +0900

    [Feature/#50] COGO 신청 api (#53)
    * feat: cogo application response 생성
    
    * chore: rename
    
    * feat: add application service
    
    * feat: api 호출
    
    * fix: response 타입 변환
    
    * feat: schedule -> memo extra 데이터 전달
    
    * feat: memo -> matching extra 데이터 전달
    
    * feat: 신청 성공시 dialog 구현

---
+++ lib/data/dto/response/cogo_application_response.dart
@@ -0,0 +1,27 @@
+class CogoApplicationResponse {
+  final int? mentorId;
+  final int? possibleDateId;
+  final String? memo;
+
+  CogoApplicationResponse({
+    required this.mentorId,
+    required this.possibleDateId,
+    required this.memo,
+  });
+
+  factory CogoApplicationResponse.fromJson(Map<String, dynamic> json) {
+    return CogoApplicationResponse(
+      mentorId: json['mentorId'],
+      possibleDateId: json['possibleDateId'],
+      memo: json['memo'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'mentorId': mentorId,
+      'possibleDateId': possibleDateId,
+      'memo': memo,
+    };
+  }
+}
--- lib/features/home/apply/view_models/matching_view_model.dart
+++ lib/features/home/apply/view_models/matching_view_model.dart
@@ -1,9 +1,100 @@
+import 'dart:developer';
+
+import 'package:cogo/common/widgets/atoms/texts/styles.dart';
+import 'package:cogo/constants/constants.dart';
+import 'package:cogo/data/service/application_service.dart';
 import 'package:flutter/material.dart';
-import 'package:cogo/constants/paths.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 
 class MatchingViewModel extends ChangeNotifier {
-  void completeApplication(BuildContext context) {
-    context.go(Paths.home);
+  final ApplicationService _applicationService = ApplicationService();
+
+  Future<void> completeApplication(BuildContext context, int mentorId,
+      int possibleDateId, String memo) async {
+    try {
+      final response = await _applicationService.postCogo(
+        mentorId,
+        possibleDateId,
+        memo,
+      );
+      log("Cogo 신청 성공: ${response.toString()}");
+
+      await _showSuccessDialog(context);
+      context.go(Paths.home);
+
+      notifyListeners();
+    } on Exception catch (e) {
+      log("Cogo 신청 실패: $e");
+    }
+  }
+
+  Future<void> _showSuccessDialog(BuildContext context) async {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double dialogWidth = screenWidth - 40;
+
+    return showDialog(
+      context: context,
+      builder: (context) => Dialog(
+        backgroundColor: Colors.white,
+        insetPadding: const EdgeInsets.all(10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: SizedBox(
+          width: dialogWidth,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // Title
+                    const Text(
+                      '커피쳇 신청이 완료되었어요!',
+                      style: CogoTextStyle.body16,
+                    ),
+                    const SizedBox(height: 8),
+                    // Subtitle
+                    Text(
+                      '멘토님께서 신청을 승인할 때까지 기다려 주세요.',
+                      style: CogoTextStyle.body12
+                          .copyWith(color: CogoColor.systemGray03),
+                    ),
+                    const SizedBox(height: 16),
+                    // Image
+                    Center(
+                      child: Image.asset(
+                        'assets/icons/3d_img/3d_rocket.png',
+                        width: 130,
+                        height: 130,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                  ],
+                ),
+              ),
+              // Close Button
+              Positioned(
+                top: -50,
+                right: 0,
+                child: GestureDetector(
+                  onTap: () => Navigator.of(context).pop(),
+                  child: SvgPicture.asset(
+                    'assets/icons/button/close.svg',
+                    width: 30,
+                    height: 30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
