f0ac6511abacdc7d5914ea47f7ba163369d51078
Seoneui <ellieella2031@gmail.com>
2024-11-10 12:23:07 +0900

    fix: memtee cogo page 라우팅 수정

--- lib/features/cogo/view_models/mentee/cogo_view_model.dart
+++ lib/features/cogo/view_models/mentee/cogo_view_model.dart
@@ -1,8 +1,11 @@
+import 'package:cogo/common/navigator/view/bottom_navigation_bar.dart';
+import 'package:cogo/constants/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CogoScreenViewModel extends ChangeNotifier {
   void navigateToReceivedCogo(BuildContext context) {
-    Navigator.pushNamed(context, '/receivedCogo');
+    context.push(Paths.sendedCogo);
   }
 
   void navigateToSuccessedCogo(BuildContext context) {
