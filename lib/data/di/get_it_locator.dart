0da5607fc922cf9941b53c68724aada923eec0ca
Yu Jin <qldls0307@naver.com>
2024-11-12 01:54:28 +0900

    fix: main에서 mentorService 의존성 제거 및 토큰 정상화

--- lib/data/di/get_it_locator.dart
+++ lib/data/di/get_it_locator.dart
@@ -1,3 +1,4 @@
+import 'package:cogo/data/service/mentor_service.dart';
 import 'package:cogo/data/service/refresh_service.dart';
 import 'package:cogo/data/service/user_service.dart';
 import 'package:cogo/features/auth/login/login_view_model.dart';
@@ -5,6 +6,7 @@ import 'package:cogo/features/auth/signup/club/club_selection_view_model.dart';
 import 'package:cogo/features/auth/signup/interest/interest_selection_view_model.dart';
 import 'package:cogo/features/auth/signup/name_input/name_input_view_model.dart';
 import 'package:cogo/features/auth/signup/phone_number/phone_number_view_model.dart';
+import 'package:cogo/features/home/home/view_model/home_view_model.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -15,6 +17,8 @@ void setupServiceLocator() {
    * dio 등록
    */
   getIt.registerLazySingleton<Dio>(() => Dio());
+
+  getIt.registerLazySingleton<MentorService>(() => MentorService());
 
   /**
    * 뷰모델 등록
@@ -33,4 +37,6 @@ void setupServiceLocator() {
 
   getIt.registerFactory<ClubSelectionViewModel>(
       () => ClubSelectionViewModel(userService: getIt<UserService>()));
+
+  getIt.registerFactory<HomeViewModel>(() => HomeViewModel());
 }
