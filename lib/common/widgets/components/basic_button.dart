1561d4d134e9e82a86ea7bc53f40c193c8f024c1
Seoneui <ellieella2031@gmail.com>
2024-11-19 16:54:42 +0900

    fix: add button width setting

--- lib/common/widgets/components/basic_button.dart
+++ lib/common/widgets/components/basic_button.dart
@@ -6,31 +6,38 @@ class BasicButton extends StatelessWidget {
   final String text;
   final bool isClickable;
   final VoidCallback? onPressed;
-  final double? width; //todo small, large
+
+  /// 버튼의 너비 (small, large)
+  final String width;
 
   const BasicButton({
     Key? key,
     required this.text,
     required this.isClickable,
     this.onPressed,
-    this.width,
+    this.width = 'small', // 기본값 small
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    /// small과 large에 따른 너비 설정
+    final double buttonWidth = width == 'small' ? 150 : double.infinity;
+
     return SizedBox(
-      width: width,
+      width: buttonWidth,
       height: 50,
       child: ElevatedButton(
         onPressed: isClickable ? onPressed : null,
         style: ElevatedButton.styleFrom(
-            backgroundColor:
-                isClickable ? CogoColor.main : CogoColor.systemGray02,
-            foregroundColor: isClickable ? CogoColor.white50 : CogoColor.main,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            textStyle: CogoTextStyle.body18),
+          backgroundColor:
+              isClickable ? CogoColor.systemGray05 : CogoColor.systemGray02,
+          foregroundColor:
+              isClickable ? CogoColor.white50 : CogoColor.systemGray05,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          textStyle: CogoTextStyle.body18,
+        ),
         child: Text(text),
       ),
     );
