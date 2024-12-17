dc46c393ded17e91e8e3d7e3f01f005e315523f0
Yu Jin <qldls0307@naver.com>
2024-12-17 15:52:22 +0900

    modify: 라우팅 코드 뷰 영역으로 옮기기

--- lib/features/auth/signup/interest/interest_selection_view_model.dart
+++ lib/features/auth/signup/interest/interest_selection_view_model.dart
@@ -1,60 +1,45 @@
 import 'dart:developer';
 
 import 'package:cogo/common/enums/interest.dart';
-import 'package:cogo/common/enums/role.dart';
-import 'package:cogo/constants/paths.dart';
 import 'package:cogo/data/repository/local/secure_storage_repository.dart';
 import 'package:cogo/data/service/user_service.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:get_it/get_it.dart';
 
 class InterestSelectionViewModel extends ChangeNotifier {
-  final UserService userService;
+  final UserService userService = GetIt.instance<UserService>();
 
   final SecureStorageRepository _secureStorage = SecureStorageRepository();
 
   String? role;
   Interest? selectedInterest;
+  bool isMenteeSignSuccess = false;
 
-  InterestSelectionViewModel({required this.userService}) {
+  InterestSelectionViewModel() {
     _loadPreferences();
   }
 
   void _loadPreferences() async {
-    // role = LocaleManager.instance.getStringValue('selectedRole');
-    // var roleString = await _secureStorage.readRole();
-    // role = Role.parseRole(roleString);
-    // final roleString = await _secureStorage.readRole();
-    // role = parseRole(roleString);
     role = await _secureStorage.readRole();
 
-    log(role.toString());
+    log("InterestSelectionViewModel 로컬 저장소 $role");
     notifyListeners();
   }
 
-  void selectInterest(BuildContext context, Interest interest) async {
+  void selectInterest(Interest interest) async {
     log(interest.name);
     selectedInterest = interest;
     notifyListeners();
 
     await _secureStorage.saveInterest(interest.name);
-    // await LocaleManager.instance.setStringValue('SelectedInterest', interest);
-
-    if (role == Role.MENTOR.name) {
-      context.push('${Paths.agreement}/${Paths.mentorClub}');
-    } else if (role == Role.MENTEE.name) {
-      log("멘티입니다");
-      signUpMentee(context, interest);
-      context.push('${Paths.agreement}/${Paths.completion}');
-    }
   }
 
-  Future<void> signUpMentee(BuildContext context, Interest interest) async {
+  Future<void> signUpMentee(Interest interest) async {
     try {
       //잘 전송이 되어야 넘어감
       await userService.signUpMentee(interest.name);
-      context.push('${Paths.agreement}/${Paths.completion}');
+      isMenteeSignSuccess = true;
 
       notifyListeners();
     } catch (e) {
