9c9b1b91c96c56c6c5609dfbbb9cba0666b4569b
Yu Jin <qldls0307@naver.com>
2024-12-18 14:33:47 +0900

    feat: 이미지 업로드 코드 dio

--- lib/features/mypage/image_upload_view_model.dart
+++ lib/features/mypage/image_upload_view_model.dart
@@ -3,13 +3,15 @@ import 'dart:io';
 
 import 'package:cogo/constants/apis.dart';
 import 'package:cogo/data/repository/local/secure_storage_repository.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_config/flutter_config.dart';
-import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
 class ImageUploadViewModel extends ChangeNotifier {
   final SecureStorageRepository _secureStorage = SecureStorageRepository();
+  final Dio _dio = Dio();
+
   File? _selectedImage;
   bool _isUploading = false;
   String? _uploadResult;
@@ -17,11 +19,8 @@ class ImageUploadViewModel extends ChangeNotifier {
 
   // Getters
   File? get selectedImage => _selectedImage;
-
   bool get isUploading => _isUploading;
-
   String? get uploadResult => _uploadResult;
-
   String? get errorMessage => _errorMessage;
 
   // 갤러리에서 이미지 선택
@@ -68,7 +67,6 @@ class ImageUploadViewModel extends ChangeNotifier {
     }
 
     try {
-      //todo dio+service로 바꾸기
       // 업로드 시작
       _isUploading = true;
       _errorMessage = null;
@@ -76,38 +74,50 @@ class ImageUploadViewModel extends ChangeNotifier {
 
       // API 엔드포인트 URL
       const apiVersion = "api/v2/";
-      final url =
-          Uri.parse('${FlutterConfig.get("base_url")}$apiVersion${Apis.s3}/v2');
+      final url = '${FlutterConfig.get("base_url")}$apiVersion${Apis.s3}/v2';
 
-      // Multipart 요청 생성
-      final req = http.MultipartRequest('POST', url);
+      // 토큰 가져오기
       var token = await _secureStorage.readAccessToken();
 
-      // 헤더 설정
-      req.headers['accept'] = '*/*';
-      req.headers['Authorization'] = 'Bearer $token';
-      req.headers['Content-Type'] = 'multipart/form-data';
+      // FormData 생성
+      final formData = FormData.fromMap({
+        'image': await MultipartFile.fromFile(_selectedImage!.path,
+            filename: _selectedImage!.path.split('/').last)
+      });
 
-      // 이미지 파일 추가
-      req.files.add(
-          await http.MultipartFile.fromPath('image', _selectedImage!.path));
+      // Dio 요청 옵션 설정
+      final options = Options(
+        headers: {
+          'accept': '*/*',
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'multipart/form-data',
+        },
+      );
 
       // 요청 전송
-      final stream = await req.send();
-      final res = await http.Response.fromStream(stream);
+      final response = await _dio.post(
+        url,
+        data: formData,
+        options: options,
+      );
 
       // 상태 코드 확인
-      if (res.statusCode != 201) {
-        throw Exception('업로드 실패: 상태 코드 = ${res.statusCode}');
+      if (response.statusCode != 201) {
+        throw Exception('업로드 실패: 상태 코드 = ${response.statusCode}');
       }
 
       // 업로드 성공
-      _uploadResult = res.body;
+      _uploadResult = response.data.toString();
       log(_uploadResult.toString());
       _isUploading = false;
       notifyListeners();
+    } on DioException catch (e) {
+      // Dio 특화된 에러 처리
+      _errorMessage = '업로드 중 오류 발생: ${e.response?.data ?? e.message}';
+      _isUploading = false;
+      notifyListeners();
     } catch (e) {
-      // 에러 처리
+      // 기타 예외 처리
       _errorMessage = '업로드 중 오류 발생: ${e.toString()}';
       _isUploading = false;
       notifyListeners();
