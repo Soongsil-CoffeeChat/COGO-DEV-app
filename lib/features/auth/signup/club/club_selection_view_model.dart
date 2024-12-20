f2cd084920a761eb0b21ecf32897a68dfbd4ee5b
Yu Jin <qldls0307@naver.com>
2024-12-20 14:14:55 +0900

    chore: 불필요한 코드 제거

--- lib/features/auth/signup/club/club_selection_view_model.dart
+++ lib/features/auth/signup/club/club_selection_view_model.dart
@@ -1,23 +1,13 @@
-import 'dart:developer';
-
 import 'package:cogo/data/repository/local/secure_storage_repository.dart';
 import 'package:flutter/material.dart';
 
 class ClubSelectionViewModel extends ChangeNotifier {
-  String? selectedClub;
-  String? interest;
   final SecureStorageRepository _secureStorage = SecureStorageRepository();
 
   ClubSelectionViewModel();
 
   Future<void> selectClub(String club) async {
-    selectedClub = club;
-    notifyListeners();
-
     _secureStorage.saveClub(club);
-
-    interest = await _secureStorage.readInterest();
-
-    log("ClubSelectionViewModel selectedClub: $selectedClub interest $interest");
+    notifyListeners();
   }
 }
