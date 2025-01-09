74faa5e25968eb6415b501ab4e02c1f7b63a04bf
Seoneui <ellieella2031@gmail.com>
2025-01-10 04:13:34 +0900

    design: snackbar 배경색, 폰트 변경

--- lib/features/mypage/my_info/my_info_screen.dart
+++ lib/features/mypage/my_info/my_info_screen.dart
@@ -94,47 +94,50 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
 
                           /// 휴대폰 인증번호 받기 클릭 시 나오는 인증 번호 text field
                           if (viewModel.showVerificationField.value)
-                            Row(
+                            Column(
                               children: [
-                                Expanded(
-                                  /// 휴대폰 인증 번호 text field
-                                  child: SecondaryTextfield(
-                                    controller: viewModel
-                                        .phoneVerificationCodeController,
-                                    keyboardType: TextInputType.number,
-                                    labelText: '인증번호',
-                                  ),
-                                ),
-                                const SizedBox(width: 10),
+                                Row(
+                                  children: [
+                                    Expanded(
+                                      /// 휴대폰 인증 번호 text field
+                                      child: SecondaryTextfield(
+                                        controller: viewModel
+                                            .phoneVerificationCodeController,
+                                        keyboardType: TextInputType.number,
+                                        labelText: '인증번호',
+                                      ),
+                                    ),
+                                    const SizedBox(width: 10),
 
-                                /// 타이머
-                                Text(
-                                  _formatTime(viewModel
-                                      .remainingSeconds), // 남은 시간을 mm:ss 형태로 포맷
-                                  style: CogoTextStyle.bodyL12
-                                      .copyWith(color: Colors.red),
-                                ),
-                                const SizedBox(width: 10),
+                                    /// 타이머
+                                    Text(
+                                      _formatTime(viewModel.remainingSeconds),
+                                      style: CogoTextStyle.bodyL12
+                                          .copyWith(color: Colors.red),
+                                    ),
+                                    const SizedBox(width: 10),
 
-                                /// 확인 버튼
-                                ThirdButton(
-                                  onPressed: () {
-                                    viewModel.checkPhoneVerificationCode();
-                                    if (viewModel
-                                        .successPhoneVerificationCode) {
-                                      showSuccessSnackbar(
-                                          context, '휴대폰 번호 인증에 성공하였습니다.');
-                                    } else {
-                                      showSuccessSnackbar(
-                                          context, '인증번호가 일치하지 않습니다.');
-                                    }
-                                  },
-                                  text: "확인",
-                                  isClickable: true,
+                                    /// 확인 버튼
+                                    ThirdButton(
+                                      onPressed: () {
+                                        viewModel.checkPhoneVerificationCode();
+                                        if (viewModel
+                                            .successPhoneVerificationCode) {
+                                          showSnackbar(
+                                              context, '휴대폰 번호 인증에 성공하였습니다.');
+                                        } else {
+                                          showSnackbar(
+                                              context, '인증번호가 일치하지 않습니다.');
+                                        }
+                                      },
+                                      text: "확인",
+                                      isClickable: true,
+                                    ),
+                                  ],
                                 ),
+                                const SizedBox(height: 20),
                               ],
                             ),
-                          const SizedBox(height: 20),
 
                           /// 이메일 필드 + 인증번호 받기 버튼
                           Row(
@@ -180,8 +183,7 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
 
                                 /// 타이머
                                 Text(
-                                  _formatTime(viewModel
-                                      .remainingEmailSeconds), // 남은 시간을 mm:ss 형태로 포맷
+                                  _formatTime(viewModel.remainingEmailSeconds),
                                   style: CogoTextStyle.bodyL12
                                       .copyWith(color: Colors.red),
                                 ),
@@ -193,11 +195,9 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
                                     viewModel.checkEmailVerificationCode();
                                     if (viewModel
                                         .successEmailVerificationCode) {
-                                      showSuccessSnackbar(
-                                          context, '이메일 인증에 성공하였습니다.');
+                                      showSnackbar(context, '이메일 인증에 성공하였습니다.');
                                     } else {
-                                      showSuccessSnackbar(
-                                          context, '인증번호가 일치하지 않습니다.');
+                                      showSnackbar(context, '인증번호가 일치하지 않습니다.');
                                     }
                                   },
                                   text: "확인",
@@ -214,7 +214,6 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
               ),
 
               /// 저장하기 버튼
-              /// 따로 Cusumer 구독을 해야지 상태가 변경됨 왜지???
               Consumer<MyInfoViewModel>(
                 builder: (context, viewModel, child) {
                   return Padding(
@@ -236,20 +235,21 @@ class _MyInfoScreenState extends State<MyInfoScreen> {
     );
   }
 
-  void showSuccessSnackbar(BuildContext context, String text) {
+  /// 인증 완료 다이얼로그
+  void showSnackbar(BuildContext context, String text) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(text),
-        backgroundColor: Colors.green,
+        content: Text(text, style: CogoTextStyle.body14),
+        backgroundColor: Colors.white,
         duration: const Duration(seconds: 4),
       ),
     );
   }
 
+  /// 타이머 format 설정
   String _formatTime(int seconds) {
     final minutes = seconds ~/ 60;
     final secs = seconds % 60;
-    // 예: 2:05 / 0:08 이런 식으로
     return '$minutes:${secs.toString().padLeft(2, '0')}';
   }
 }
