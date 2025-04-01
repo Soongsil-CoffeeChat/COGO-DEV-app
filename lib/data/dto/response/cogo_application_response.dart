2d37f831e82061852a1616eac2824962ae10526f
Seoneui <ellieella2031@gmail.com>
2025-04-01 17:15:10 +0900

    refactor: response freezed 형식 변경

--- lib/data/dto/response/cogo_application_response.dart
+++ lib/data/dto/response/cogo_application_response.dart
@@ -6,9 +6,13 @@ part 'cogo_application_response.g.dart';
 @freezed
 class CogoApplicationResponse with _$CogoApplicationResponse {
   const factory CogoApplicationResponse({
-    int? mentorId,
-    int? possibleDateId,
-    String? memo,
+    @JsonKey(name: 'applicationId') required int applicationId,
+    @JsonKey(name: 'mentorId') required int mentorId,
+    @JsonKey(name: 'menteeId') required int menteeId,
+    @JsonKey(name: 'applicationMemo') required String applicationMemo,
+    @JsonKey(name: 'applicationDate') required String applicationDate,
+    @JsonKey(name: 'applicationStartTime') required String applicationStartTime,
+    @JsonKey(name: 'applicationEndTime') required String applicationEndTime,
   }) = _CogoApplicationResponse;
 
   factory CogoApplicationResponse.fromJson(Map<String, dynamic> json) =>
--- lib/data/dto/response/cogo_info_response.dart
+++ lib/data/dto/response/cogo_info_response.dart
@@ -6,13 +6,13 @@ part 'cogo_info_response.g.dart';
 @freezed
 class CogoInfoResponse with _$CogoInfoResponse {
   const factory CogoInfoResponse({
-    required int applicationId,
-    required String menteeName,
-    required String mentorName,
-    required String applicationMemo,
-    required String applicationDate,
-    required String applicationStartTime,
-    required String applicationEndTime,
+    @JsonKey(name: 'applicationId') required int applicationId,
+    @JsonKey(name: 'menteeName') required String menteeName,
+    @JsonKey(name: 'mentorName') required String mentorName,
+    @JsonKey(name: 'applicationMemo') required String applicationMemo,
+    @JsonKey(name: 'applicationDate') required String applicationDate,
+    @JsonKey(name: 'applicationStartTime') required String applicationStartTime,
+    @JsonKey(name: 'applicationEndTime') required String applicationEndTime,
   }) = _CogoInfoResponse;
 
   factory CogoInfoResponse.fromJson(Map<String, dynamic> json) =>
