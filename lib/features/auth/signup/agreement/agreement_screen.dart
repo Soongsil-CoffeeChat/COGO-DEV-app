ad3381b7b4e85990bf1a0e8a11fdf13395830029
Seoneui <ellieella2031@gmail.com>
2025-04-17 14:27:24 +0900

    design: 개인정보동의 바텀시트 디자인 변경

--- lib/features/auth/signup/agreement/agreement_screen.dart
+++ lib/features/auth/signup/agreement/agreement_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cogo/common/widgets/components/basic_button.dart';
 import 'package:cogo/constants/paths.dart';
 import 'package:cogo/features/auth/signup/agreement/agreement_view_model.dart';
+import 'package:cogo/features/auth/signup/agreement/terms_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -25,21 +26,8 @@ class _AgreementScreenState extends State<AgreementScreen> {
     showModalBottomSheet(
       context: context,
       isScrollControlled: true,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-      ),
       builder: (context) {
-        return ChangeNotifierProvider(
-          create: (_) => AgreementViewModel(),
-          child: Padding(
-            padding: EdgeInsets.only(
-              bottom: MediaQuery.of(context).viewInsets.bottom,
-            ),
-            child: SafeArea(
-              child: agreementBottomSheet(context),
-            ),
-          ),
-        );
+        return const TermsBottomSheet();
       },
     );
   }
@@ -48,120 +36,6 @@ class _AgreementScreenState extends State<AgreementScreen> {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.white,
-    );
-  }
-}
-
-Widget agreementBottomSheet(BuildContext context) {
-  return Container(
-    padding: const EdgeInsets.all(20),
-    decoration: const BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(20),
-        topRight: Radius.circular(20),
-      ),
-    ),
-    child: Consumer<AgreementViewModel>(
-      builder: (context, viewModel, child) => Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CheckBoxRow(
-            index: 0,
-            title: '서비스 이용약관 ',
-            mandatoryText: '(필수)',
-          ),
-          CheckBoxRow(
-            index: 1,
-            title: '개인정보 수집 및 이용 동의',
-            mandatoryText: '(필수)',
-          ),
-          CheckBoxRow(
-            index: 2,
-            title: '만 14세 이상 ',
-            mandatoryText: '(필수)',
-          ),
-          CheckBoxRow(
-            index: 3,
-            title: '서비스 알림 수신 ',
-            mandatoryText: '(필수)',
-          ),
-          CheckBoxRow(
-            index: 4,
-            title: '서비스 혜택 정보 수신 ',
-            mandatoryText: '(선택)',
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0), // 상단에만 20의 패딩 추가
-            child: BasicButton(
-              text: '동의하고 시작하기',
-              isClickable: viewModel.isAllRequiredChecked,
-              onPressed: () {
-                if (viewModel.isAllRequiredChecked) {
-                  context.push('${Paths.agreement}/${Paths.phone}');
-                }
-              },
-              size: BasicButtonSize.LARGE,
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
-}
-
-class CheckBoxRow extends StatelessWidget {
-  final int index;
-  final String title;
-  final String mandatoryText;
-
-  CheckBoxRow({
-    required this.index,
-    required this.title,
-    required this.mandatoryText,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<AgreementViewModel>(
-      builder: (context, viewModel, child) => Row(
-        children: [
-          Checkbox(
-            value: viewModel.isChecked[index],
-            onChanged: (value) {
-              viewModel.updateCheckState(index, value!);
-            },
-            activeColor: Colors.black,
-            checkColor: Colors.white,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 5.0),
-            child: RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: title,
-                    style: const TextStyle(
-                      fontFamily: 'PretendardRegular',
-                      fontSize: 12,
-                      color: Colors.black, // 기본 색상
-                    ),
-                  ),
-                  TextSpan(
-                    text: mandatoryText,
-                    style: const TextStyle(
-                      fontFamily: 'PretendardRegular',
-                      fontSize: 12,
-                      color: Color(0xFF808080),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
