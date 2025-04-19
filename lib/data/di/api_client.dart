fb5093518ecbddeef03cbfcbbc8c1f63fa129603
Seoneui Jee <146940671+sunnny619@users.noreply.github.com>
2025-04-19 17:54:14 +0900

    [Feat/token interceptor] 토큰 재발급 401 interceptor (#110)
    * feat: 토큰 만료시 interceptor 구현
    
    * fix: error interceptor 수정
    
    * fix: 홈 텝바 누를때마다 api 호출 누락 코드 추가

--- lib/data/di/api_client.dart
+++ lib/data/di/api_client.dart
@@ -1,17 +1,16 @@
 import 'dart:developer';
 
 import 'package:cogo/data/repository/local/secure_storage_repository.dart';
+import 'package:cogo/data/service/auth_service.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:get_it/get_it.dart';
 
 class ApiClient {
-  static final ApiClient _instance = ApiClient._internal(); //싱글톤으로 인스턴스 생성
+  static final ApiClient _instance = ApiClient._internal();
   late Dio _dio;
   final SecureStorageRepository _secureStorage = SecureStorageRepository();
-
-  factory ApiClient() {
-    return _instance;
-  }
+  factory ApiClient() => _instance;
 
   ApiClient._internal() {
     _dio = Dio(BaseOptions(
@@ -42,14 +41,51 @@ class ApiClient {
         log('응답은요 : ${response.statusCode}');
         return handler.next(response);
       },
-      onError: (DioException e, handler) {
-        // 에러 처리
-        log('Error: ${e.message}');
-        return handler.next(e);
+      onError: (DioException e, handler) async {
+        // JWT_003 체크
+        final errorCode = e.response?.data['error'];
+        final requestOptions = e.requestOptions;
+
+        if (e.response?.statusCode == 401) {
+          try {
+            log('[Interceptor] 401 → 토큰 재발급 시도 중...');
+
+            final authService = GetIt.instance<AuthService>();
+            await authService.reissueToken();
+
+            // 새로운 access token 가져오기
+            final newToken = await _secureStorage.readAccessToken();
+
+            // 이전 요청 복사 후 헤더 수정 및 재시도 플래그 추가
+            final clonedRequest = await _dio.request(
+              requestOptions.path,
+              data: requestOptions.data,
+              queryParameters: requestOptions.queryParameters,
+              options: Options(
+                method: requestOptions.method,
+                headers: {
+                  ...requestOptions.headers,
+                  'Authorization': 'Bearer $newToken',
+                },
+                extra: {
+                  ...requestOptions.extra,
+                  'retry': true,
+                },
+              ),
+            );
+
+            return handler.resolve(clonedRequest); // 성공 응답으로 대체
+          } catch (refreshError) {
+            log('[Interceptor] 토큰 재발급 실패: $refreshError');
+            return handler.reject(e); // 기존 에러 유지
+          }
+        }
+
+        return handler.next(e); // 다른 에러는 그대로
       },
     ));
 
-    // Log Interceptor 추가
+    /// Log Interceptor 추가
     _dio.interceptors.add(LogInterceptor(
       request: true,
       requestHeader: true,
@@ -62,6 +98,5 @@ class ApiClient {
       },
     ));
   }
-
   Dio get dio => _dio;
 }
