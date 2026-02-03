ed0955b2f1e89a3fcb84d7aece644932b915fae3
Seoneui Jee <ellieella2031@gmail.com>
2026-02-03 22:49:33 +0900

    refactor: 전화번호인증 문자 번호 클립보드에서 추천 기능 추가
    autofullhint 사용

--- lib/features/auth/signup/phone_number/phone_number_screen.dart
+++ lib/features/auth/signup/phone_number/phone_number_screen.dart
@@ -28,135 +28,154 @@ class PhoneNumberScreen extends StatelessWidget {
                     padding: const EdgeInsets.all(16),
                     child: ConstrainedBox(
                       constraints:
-                          BoxConstraints(minHeight: constraints.maxHeight),
+                      BoxConstraints(minHeight: constraints.maxHeight),
                       child: IntrinsicHeight(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Header(
-                              title: '휴대폰 번호를 입력해주세요',
-                              subtitle: '개인정보는 정보통신망법에 따라 안전하게 보관됩니다',
-                              onBackButtonPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 24.0),
-                              child: TextField(
-                                controller: viewModel.phoneController,
-                                keyboardType: TextInputType.phone,
-                                inputFormatters: [
-                                  FilteringTextInputFormatter.digitsOnly,
-                                  PhoneNumberInputFormatter(),
-                                ],
-                                decoration: const InputDecoration(
-                                  labelText: '휴대폰 번호',
-                                  labelStyle: TextStyle(color: Colors.grey),
-                                  enabledBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.black),
-                                  ),
-                                  focusedBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.black),
+                        child: AutofillGroup(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Header(
+                                title: '휴대폰 번호를 입력해주세요',
+                                subtitle: '개인정보는 정보통신망법에 따라 안전하게 보관됩니다',
+                                onBackButtonPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 24.0),
+                                child: TextField(
+                                  controller: viewModel.phoneController,
+                                  keyboardType: TextInputType.phone,
+                                  autofillHints: const [
+                                    AutofillHints.telephoneNumber
+                                  ],
+                                  inputFormatters: [
+                                    FilteringTextInputFormatter.digitsOnly,
+                                    PhoneNumberInputFormatter(),
+                                  ],
+                                  decoration: const InputDecoration(
+                                    labelText: '휴대폰 번호',
+                                    labelStyle: TextStyle(color: Colors.grey),
+                                    enabledBorder: UnderlineInputBorder(
+                                      borderSide:
+                                      BorderSide(color: Colors.black),
+                                    ),
+                                    focusedBorder: UnderlineInputBorder(
+                                      borderSide:
+                                      BorderSide(color: Colors.black),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            ValueListenableBuilder<String?>(
-                              valueListenable: viewModel.errorMessage,
-                              builder: (context, errorMessage, child) {
-                                return errorMessage == null
-                                    ? const SizedBox.shrink()
-                                    : Padding(
-                                        padding:
-                                            const EdgeInsets.only(top: 8.0),
-                                        child: Text(
-                                          errorMessage,
-                                          style: const TextStyle(
-                                            color: Colors.red,
-                                            fontSize: 12,
-                                            fontFamily: 'PretendardMedium',
-                                          ),
-                                        ),
-                                      );
-                              },
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 34.0),
-                              child: ValueListenableBuilder<bool>(
-                                valueListenable:
-                                    viewModel.showVerificationField,
-                                builder: (context, show, child) {
-                                  if (show) {
-                                    return Column(
-                                      children: [
-                                        TextField(
-                                          controller: viewModel.codeController,
-                                          keyboardType: TextInputType.number,
-                                          decoration: const InputDecoration(
-                                            labelText: '인증번호',
-                                            labelStyle:
-                                                TextStyle(color: Colors.grey),
-                                            enabledBorder: UnderlineInputBorder(
-                                              borderSide: BorderSide(
-                                                  color: Colors.black),
-                                            ),
-                                            focusedBorder: UnderlineInputBorder(
-                                              borderSide: BorderSide(
-                                                  color: Colors.black),
-                                            ),
-                                          ),
-                                        ),
-                                        const SizedBox(height: 32),
-                                      ],
-                                    );
-                                  }
-                                  return const SizedBox.shrink();
+                              ValueListenableBuilder<String?>(
+                                valueListenable: viewModel.errorMessage,
+                                builder: (context, errorMessage, child) {
+                                  return errorMessage == null
+                                      ? const SizedBox.shrink()
+                                      : Padding(
+                                    padding:
+                                    const EdgeInsets.only(top: 8.0),
+                                    child: Text(
+                                      errorMessage,
+                                      style: const TextStyle(
+                                        color: Colors.red,
+                                        fontSize: 12,
+                                        fontFamily: 'PretendardMedium',
+                                      ),
+                                    ),
+                                  );
                                 },
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 34),
-                              child: Center(
+                              Padding(
+                                padding: const EdgeInsets.only(top: 34.0),
                                 child: ValueListenableBuilder<bool>(
-                                  valueListenable: viewModel.isValidPhoneNumber,
-                                  builder: (context, isValid, child) {
-                                    return ValueListenableBuilder<bool>(
-                                      valueListenable: viewModel.isValidCode,
-                                      builder: (context, isValidCode, child) {
-                                        return BasicButton(
-                                          text: viewModel.isPhoneNumberSubmitted
-                                              ? '확인'
-                                              : '인증 번호 받기',
-                                          isClickable: isValid &&
-                                              (!viewModel.showVerificationField
-                                                      .value ||
-                                                  isValidCode),
-                                          onPressed: (isValid &&
-                                                  (!viewModel
-                                                          .showVerificationField
-                                                          .value ||
-                                                      isValidCode))
-                                              ? () {
-                                                  if (viewModel
-                                                      .isPhoneNumberSubmitted) {
-                                                    viewModel
-                                                        .onVerificationCodeSubmitted(
-                                                            context);
-                                                  } else {
-                                                    viewModel
-                                                        .onPhoneNumberSubmitted();
-                                                  }
-                                                }
-                                              : null,
-                                          size: BasicButtonSize.SMALL,
-                                        );
-                                      },
-                                    );
+                                  valueListenable:
+                                  viewModel.showVerificationField,
+                                  builder: (context, show, child) {
+                                    if (show) {
+                                      return Column(
+                                        children: [
+                                          TextField(
+                                            controller:
+                                            viewModel.codeController,
+                                            keyboardType: TextInputType.number,
+                                            // [수정 3] 인증번호(OTP) 자동 완성 힌트 추가
+                                            autofillHints: const [
+                                              AutofillHints.oneTimeCode
+                                            ],
+                                            decoration: const InputDecoration(
+                                              labelText: '인증번호',
+                                              labelStyle: TextStyle(
+                                                  color: Colors.grey),
+                                              enabledBorder:
+                                              UnderlineInputBorder(
+                                                borderSide: BorderSide(
+                                                    color: Colors.black),
+                                              ),
+                                              focusedBorder:
+                                              UnderlineInputBorder(
+                                                borderSide: BorderSide(
+                                                    color: Colors.black),
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(height: 32),
+                                        ],
+                                      );
+                                    }
+                                    return const SizedBox.shrink();
                                   },
                                 ),
                               ),
-                            ),
-                          ],
+                              Padding(
+                                padding: const EdgeInsets.only(top: 34),
+                                child: Center(
+                                  child: ValueListenableBuilder<bool>(
+                                    valueListenable:
+                                    viewModel.isValidPhoneNumber,
+                                    builder: (context, isValid, child) {
+                                      return ValueListenableBuilder<bool>(
+                                        valueListenable:
+                                        viewModel.isValidCode,
+                                        builder:
+                                            (context, isValidCode, child) {
+                                          return BasicButton(
+                                            text: viewModel
+                                                .isPhoneNumberSubmitted
+                                                ? '확인'
+                                                : '인증 번호 받기',
+                                            isClickable: isValid &&
+                                                (!viewModel
+                                                    .showVerificationField
+                                                    .value ||
+                                                    isValidCode),
+                                            onPressed: (isValid &&
+                                                (!viewModel
+                                                    .showVerificationField
+                                                    .value ||
+                                                    isValidCode))
+                                                ? () {
+                                              if (viewModel
+                                                  .isPhoneNumberSubmitted) {
+                                                viewModel
+                                                    .onVerificationCodeSubmitted(
+                                                    context);
+                                              } else {
+                                                viewModel
+                                                    .onPhoneNumberSubmitted();
+                                              }
+                                            }
+                                                : null,
+                                            size: BasicButtonSize.SMALL,
+                                          );
+                                        },
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
